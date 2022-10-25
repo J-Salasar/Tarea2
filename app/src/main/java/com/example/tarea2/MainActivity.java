f19849da285e17ebec561e38a53b50f3b9e52462
Salasar001 <salasar1820@gmail.com>
2022-10-25 07:15:11 -0600

    Aplicacion terminada

--- app/src/main/java/com/example/tarea2/MainActivity.java
+++ app/src/main/java/com/example/tarea2/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.tarea2;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
-
+    private EditText nombre_txt,apellido_txt,edad_txt,correo_txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nombre_txt=(EditText) findViewById(R.id.txt_nombre);
+        apellido_txt=(EditText) findViewById(R.id.txt_apellido);
+        edad_txt=(EditText) findViewById(R.id.txt_edad);
+        correo_txt=(EditText) findViewById(R.id.txt_correo);
+    }
+    public void enviar(View view){
+        Intent intent=new Intent(this,ActivityInformacion.class);
+        intent.putExtra("nombre",nombre_txt.getText().toString());
+        intent.putExtra("apellido",apellido_txt.getText().toString());
+        intent.putExtra("edad",edad_txt.getText().toString());
+        intent.putExtra("correo",correo_txt.getText().toString());
+        startActivity(intent);
+        nombre_txt.setText("");
+        apellido_txt.setText("");
+        edad_txt.setText("");
+        correo_txt.setText("");
     }
 }
